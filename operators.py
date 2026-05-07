389a07e91046f8fea60295a7e2faaedf2516783d
KeshWrld01 <124397226+KeshWrld01@users.noreply.github.com>
2026-05-07 12:23:42 +0300

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,33 @@
+tree1 = 45
+tree2 = 54
+tree3 = 56
+tree4 = 38
+
+total = tree1 + tree2 + tree3 + tree4
+print("The total height: ", total)
+average = total / 4
+print("The average height is: ", average)
+
+#modulus
+x = 14
+y = 5
+z = x % y
+print("Moduls:", z)
+a = x // y
+print("Floor division: ",a)
+b = 2**4
+print("Exponential: ",b)
+
+maths = 70
+english = 50
+computer = 60
+design_tech = 50
+
+total = maths + english + computer + design_tech
+print("Total marks:",total)
+
+percentage = (total/400) * 100
+print("Percentage: ",percentage)
+
+z = 25 ** (0.5)
+print(z)
